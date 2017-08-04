f1b2d9b8abb55eecad129d91ee22fc8e17b4856d
Z <Rizomych>
2017-08-04 16:43:16 +0300

    add subscription, notifications, footer

--- js/custom.js
+++ js/custom.js
@@ -58,7 +58,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Ваша паведамленне паспяхова адпраўлена</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -70,7 +70,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Выбачайце " + firstName + ", мэйл сэрвер не адказвае. Калі ласка паспрабуйце адправіць паведамленне пазней! Або скарыстайце адрас lauka@falanster.by");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
