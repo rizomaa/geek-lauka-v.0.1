1011cddb950ecfef874784dca228daa0ede0d6d3
Ryzomych <rizoma@mail.riri>
2017-08-07 14:03:32 +0300

    added meta destription and files for prodict + mailing encoding

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'falanster.by@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Паведамленне з Гік-лаўкі ад $name";
 $email_body = "Змест паведамлення.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+
+$headers = "From: $email_address\r\n".
+		"MIME-Version: 1.0" . "\r\n" . 
+       	        "Content-type: text/html; charset=UTF-8" . "\r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
