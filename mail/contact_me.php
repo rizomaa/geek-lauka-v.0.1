0bdabff625d5b5f3060ad1bb15ddeb875a5f4886
Mister Cable <fannrm@gmail.com>
2018-01-12 22:11:23 +0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,8 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'team@geeklauka.by'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'soonreal+xx5ebhd5no5uw9cvltnh@boards.trello.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+//previous email team@geeklauka.by
 $email_subject = "Паведамленне з Гік-лаўкі ад $name";
 $email_body = "Змест паведамлення.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 
