6bb45ff13081ba8cb61bcb3aa68889aac87f5dd0
pro2s <protasov.s.s@gmail.com>
2018-04-14 14:51:45 +0300

    add jqueryvalidation plugin
    use old validation only on contact page

--- js/custom.js
+++ js/custom.js
@@ -16,100 +16,52 @@ jQuery(document).ready(function( $ ) {
       $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
       return false;
   });
+  
+  $.validator.addMethod("specificName", function(value, element) {
+    return this.optional(element) || /^[A-Za-zА-Яа-яЁё]{1,22}$/.test(value);
+  }, "Дапушчальныя толькі рускія і лацінскія сімвалы, не больш за 20");
+  
+  $.extend(jQuery.validator.messages, {
+    required: "Поле павінна быць запоўнена",
+    email: "Памылковы фармат адрасу"
+  });
+  
+  $( "#mc-embedded-subscribe-form" ).validate( {
+      ignoreTitle: true,
+      rules: {
+        FNAME: {
+          required: true,
+          specificName: true
+        },
+        LNAME: {
+          required: true,
+          specificName: true
+        },
+        EMAIL: {
+            required: true,
+            email: true
+        },
+      },
+      errorElement: "em",
+      errorPlacement: function ( error, element ) {
+          // Add the `help-block` class to the error element
+          error.addClass( "help-block" );
+
+          if ( element.prop( "type" ) === "checkbox" ) {
+              error.insertAfter( element.parent( "label" ) );
+          } else {
+              error.insertAfter( element );
+          }
+      },
+      highlight: function ( element, errorClass, validClass ) {
+          $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
+      },
+      unhighlight: function (element, errorClass, validClass) {
+          $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
+      }
+  });
 
 });
-
-
-$(function() {
-    function showSuccess() {
-        // Enable button & show success message
-        $("#btnSubmit").attr("disabled", false);
-        $('#success').html("<div class='alert alert-success'>");
-        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-        $('#success > .alert-success')
-            .append("<strong>Ваша паведамленне паспяхова адпраўлена</strong>");
-        $('#success > .alert-success')
-            .append('</div>');
-    }
-    function showError(message) {
-        // Fail message
-        $('#success').html("<div class='alert alert-danger'>");
-        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-        $('#success > .alert-danger').append("<strong>" + message + "</strong>");
-        $('#success > .alert-danger').append('</div>');
-    }
-
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            // Prevent spam click and default submit behaviour
-            $("#btnSubmit").attr("disabled", true);
-            event.preventDefault();
-            
-            // get values from FORM
-            var captcha = $('#g-recaptcha-response').val();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                    'g-recaptcha-response': captcha
-                },
-                cache: false,
-                success: function(data) {
-                    var json = $.parseJSON(data); 
-                    if('result' in json && json.result == 'error') {
-                        showError("Памылка ў уведзеных дадзеных");
-                    } else {
-                        showSuccess();
-                        //clear all fields
-                        $('#contactForm').trigger("reset");
-                        //clear recaptcha
-                        if(typeof grecaptcha != 'undefined') {
-                            grecaptcha.reset();
-                        }
-                    }
-                },
-                error: function() {
-                    showError("Выбачайце " + firstName + ", мэйл сэрвер не адказвае. Калі ласка паспрабуйце адправіць паведамленне пазней! Або скарыстайце адрас lauka@falanster.by");
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
-
-// When clicking on Full hide fail/success boxes
-$('#name').focus(function() {
-    $('#success').html('');
-});
-
 
 //  Google analysis
 
